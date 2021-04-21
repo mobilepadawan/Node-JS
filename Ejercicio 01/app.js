073118a705ee7170ed449b7d5e0e9695ecd7f6c5
Fernando O <fernando@vidamobile.com.ar>
2021-04-21 12:02:42 -0300

    Exercise no. 1

---
+++ Ejercicio 01/app.js
@@ -0,0 +1,10 @@
+const frutas = ['Banana', 'Manzana', 'Pera', 'Banana', 'Frutilla', 'Pomelo']
+
+frutas.forEach((item => {
+   console.count(item)
+}))
+ 
+//Ejecutar en la Consola o Terminal la siguiente línea:
+//node app.js
+
+//Deberá listar los elementos del array frutas.
