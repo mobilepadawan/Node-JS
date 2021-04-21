81fe43de6dfe1582db6bbf839694c71939f50a8a
Fernando O <fernando@vidamobile.com.ar>
2021-04-21 13:03:41 -0300

    Ejercise 3 cowsay and npm to install dependencies

--- Ejercicio 03/app.js
+++ Ejercicio 03/app.js
@@ -2,11 +2,21 @@
 //Para crear el primer package.json en nuestro proyecto node, escribir:
 //npm init -y //en Mac no me funcionó este comando, solo utilicé npm init
 
+//Si elimino la carpeta node_modules, el archivo package.json tendrá las dependencias
+//necesarias. Luego, el usuario que descarga nuestro proyecto, debe escribir en la consola:
+//npm i
 
-const {frutas, dinero} = require('./frutas')
+//const {frutas, dinero} = require('./frutas')
+const cowsay = require('./node_modules/cowsay')
 
-frutas.forEach((item => {
-   console.count(item)
+console.log(cowsay.say({
+   text: "I'm a module installed by npm",
+   e: "Oo",
+   T: "u"
 }))
 
-console.warn(`Dinero disponible: $ ${dinero}`)
+// frutas.forEach((item => {
+//    console.count(item)
+// }))
+
+// console.warn(`Dinero disponible: $ ${dinero}`)
