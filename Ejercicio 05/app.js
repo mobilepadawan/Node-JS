5a01bdab0875b8872d3cf5e2066a35530ec29d9d
Fernando O <fernando@vidamobile.com.ar>
2022-01-12 09:10:50 -0300

    Improving error page and searching document

--- Ejercicio 05/app.js
+++ Ejercicio 05/app.js
@@ -9,14 +9,25 @@ app.set('view engine', 'ejs')
 
 //ROUTES
 app.get('/', (req, res) => {
-   res.send('Mi respuesta desde Express')
+   res.render('index')
 })
 
-app.get('/:userQuery', (req, res) => {
-   res.render('index', {data : {userQuery: req.params.userQuery, 
-                                searchResults: ['Libro 1', 'Libro 2', 'Libro 3', 'Libro 4', 'Libro 5', 'Libro 6', 'Libro 7', 'Libro 8', 'Libro 9', 'Libro 10']}})
-})
+// app.get('/buscar/', (req, res) => {
+//    res.render('buscar')
+// })
 
+app.get('buscar/:userQuery', (req, res) => {
+   if (req.params.userQuery.trim() != '') {
+      res.render('index', {data : {userQuery: req.params.userQuery, 
+         searchResults: ['Libro 1', 'Libro 2', 'Libro 3', 'Libro 4', 'Libro 5', 'Libro 6', 'Libro 7', 'Libro 8', 'Libro 9', 'Libro 10']}})
+   } else {
+      res.render('buscar')
+   }
+})
+   
+app.get('*', (req, res) => {
+   res.render('error')
+})
 
 
 app.listen(port, ()=> {
