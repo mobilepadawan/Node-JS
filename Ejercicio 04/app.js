264efc762f3d4ddef0270829d0d13c5127d446ed
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2023-05-25 18:59:41 -0300

    Delete Ejercicio 04 directory

--- Ejercicio 04/app.js
+++
@@ -1,9 +0,0 @@
-const http = require("http")
-const server = http.createServer((req, res)=> {
-   res.end("Este servidor remoto está respondiendo a tu solicitud V2.0")
-})
-const port = 3000
-
-server.listen(port, ()=> {
-   console.log("Escuchando solicitudes...")
-})
